cf058b96df4dec53356b72233caf058c9bd97a62
sowmiyams <113747188+sowmiyams@users.noreply.github.com>
2023-05-25 11:07:06 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://rnhejpbs:***@drona.db.elephantsql.com/rnhejpbs'
+SQLALCHEMY_DATABASE_URL = 'postgresql://cfvrbnsn:Vm29YEaSMKEX9Y_3GNloYNxLUkF8Rgsk@drona.db.elephantsql.com/cfvrbnsn'
 #postgresql://rnhejpbs:***@drona.db.elephantsql.com/rnhejpbs
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
